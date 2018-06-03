61766a70f10941f396b77ed99248c92dd3005242
fqwl <fqwl2016@aliyun.com>
2018-06-03 22:27:11 +0800

    修改冲突命名

---
+++ w_1.py
@@ -0,0 +1,12 @@
+import os
+import tkinter as tk
+import send2trash as st
+from tkinter import messagebox
+
+window = tk.Tk()
+
+window.title("Wallpaper Filter")
+window.geometry('260x330')
+window.resizable(False, False)
+
+window.mainloop()
